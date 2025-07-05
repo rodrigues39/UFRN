26c594184586ece32629175e0de9d4cbe0bd50f6
rodrigues39 <j.gabrielll@gmail.com>
2025-07-05 00:18:03 -0300

    Update filtered_class.py

--- PPGEEC2318/Projeto_02/data/selection_class.py
+++ PPGEEC2318/Projeto_02/data/selection_class.py
@@ -1,6 +1,25 @@
-import kagglehub
+filtered: Realizado no Google Colab com GPU 
+library: zipfile,os
+url: https://www.kaggle.com/datasets/gpiosenka/sports-classification/data
 
-# Download latest version
-path = kagglehub.dataset_download("gpiosenka/sports-classification")
+Data Generation
 
-print("Path to dataset files:", path)
+This dataset was filtered to classify 10 different sports:
+baseball, basketball, bmx, football, formula 1 racing, nascar racing, rugby, tennis, track bicycle, volleyball
+
+# Caminho do arquivo zip
+arquivo_zip = '/content/imagem_sport_10.zip'
+
+# Pasta onde o conteúdo será extraído
+pasta_destino = '/content/imagem_sport'
+
+# Criar a pasta de destino, caso não exista
+os.makedirs(pasta_destino, exist_ok=True)
+
+# Abrir o arquivo zip e extrair o conteúdo
+with zipfile.ZipFile(arquivo_zip, 'r') as zip_ref:
+    zip_ref.extractall(pasta_destino)
+
+print("Extração concluída!")
+
+
